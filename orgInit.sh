f76c75478ae248931c60076e5f145096cb581eee
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 16:36:35 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,19 +1,19 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix case -o classification.einstein
-sfdx force:source:push
-sfdx shane:user:password:set -p salesforce1 -g User -l User
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p case -e classification.workshop
+sf project deploy start
+sf demoutil user password set -p salesforce1 -g User -l User
 
 #assign  AUDIT FIELD AND Einstein Case Classification User permsets
-sfdx force:user:permset:assign --perm-set-name Audit_Fields
-sfdx force:user:permset:assign --perm-set-name EinsteinAgent
+sf org assign permset -n Audit_Fields
+sf org assign permset -n EinsteinAgent
 
 #install Einstein Case Classification Value Analytics
-sfdx force:package:install -p 04tHr000000k9NX --noprompt -w 5
+sf package install -p 04tHr000000k9NX --noprompt -w 5
 
 #bulk load Closed Cases
 sfdx shane:data:dates:update -r 09-24-2020
-sfdx force:data:bulk:upsert -s Task -f data-modified/extracttaskclean1.csv -i ID
+sf data bulk upsert -s Task -f data-modified/extracttaskclean1.csv -i ID
 
 #load task
-sfdx force:data:bulk:upsert -s Case -f data-modified/ClosedCases.csv -i External_Id__c
+sf data bulk upsert -s Case -f data-modified/ClosedCases.csv -i External_Id__c
 
-sfdx force:org:open
+sf org open
